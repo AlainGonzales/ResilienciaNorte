7a35548bb9dd810d99bf709ec004b8df0ee77e1f
Daniel27-coder <dani_alex_lazaro@hotmail.com>
2026-09-19 03:01:24 -0500

    feat(service): implementar reglas de negocio en IncidenteService y RecursoService

---
+++ ResilienciaNorte.Service/IIncidenteService.cs
@@ -0,0 +1,12 @@
+﻿using ResilienciaNorte.Domain;
+
+namespace ResilienciaNorte.Service;
+
+public interface IIncidenteService
+{
+    Task<IEnumerable<IncidenteEmergencia>> ObtenerTodosAsync(int? distritoId = null, string? estado = null);
+    Task<IncidenteEmergencia?> ObtenerPorIdAsync(int id);
+    Task<IncidenteEmergencia> RegistrarIncidenteAsync(IncidenteEmergencia incidente);
+    Task<bool> CambiarEstadoAsync(int id, string nuevoEstado);
+    Task<IEnumerable<Distrito>> ObtenerDistritosAsync();
+}
